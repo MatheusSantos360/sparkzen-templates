1113c88b31019740dbb35c50c387a5cd6ba67583
Matheus dos Santos <matheusworkcontact@gmail.com>
2025-11-30 17:18:01 +0000

    fix: remove documentation (it wasn't supposed to be there)

--- default/src/routes/users/[id]/post.ts
+++ default/src/routes/users/[id]/post.ts
@@ -1,34 +1,55 @@
 import { Middleware, T, type Handler } from "sparkzen";
 
-// Yes, POST api/users/:id/ route here!
-
 export const schema = {
-  params: T.Object({ id: T.Number() }),
+  params: T.Object({
+    id: T.Number({
+      description: "User ID",
+      examples: [1],
+    }),
+  }),
+
   body: T.Object({
-    name: T.String(),
+    name: T.String({ minLength: 2 }),
     nick: T.Optional(T.String()),
   }),
+
   response: {
-    200: T.Object({ ok: T.Boolean() }),
+    200: T.Object({
+      ok: T.Boolean(),
+      message: T.String(),
+      received: T.Object({
+        id: T.Number(),
+        name: T.String(),
+        nick: T.Optional(T.String()),
+      }),
+    }),
   },
-}; // Define schema for request and response
-
-export const middlewares: Middleware<typeof schema> = [(request, _reply, done) => {
-  request.body
-  console.log("Middleware executed");
-  done();
-}, (_request, _reply, done) => {
-  console.log("Second middleware executed");
-  done();
-}] // You can define middlewares for this route!
-
-const handler: Handler<typeof schema> = async (request, reply) => { // You can pass the schema type to Handler!
-  const { id } = request.params;        // id is of type number
-  const { name, nick } = request.body;  // name is string, nick is string | undefined  --> Schema validation!
-
-  return reply.status(200).send({ ok: true }); // Response matches the schema
 };
 
-export default handler; // Don't forget to export the handler as default (Again)!
+export const middlewares: Middleware<typeof schema> = [
+  (request, _reply, done) => {
+    console.log("[SparkZen] Incoming request:", {
+      params: request.params,
+      body: request.body,
+    });
+    done();
+  },
 
-// You can export any other options too (Fastify RouteOptions type).
+  (_request, _reply, done) => {
+    console.log("[SparkZen] Second middleware executed");
+    done();
+  },
+];
+
+const handler: Handler<typeof schema> = async (request, reply) => {
+  const { id } = request.params;
+  const { name, nick } = request.body;
+
+  return reply.status(200).send({
+    ok: true,
+    message: "User processed successfully",
+    received: { id, name, nick },
+  });
+};
+
+export default handler;
--- default/src/routes/users/get.ts
+++ default/src/routes/users/get.ts
@@ -1,8 +1,7 @@
 import type { Handler } from "sparkzen";
 
 const handler: Handler = async (request, reply) => {
-  // Do whatever you want!
   return reply.status(200).send({ message: "Hello, SparkZen!" });
 };
 
-export default handler; // Don't forget to export the handler as default!
+export default handler;
